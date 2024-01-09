ca3c2df38ca3b4da593911c266d1aebe5df0e24e
panda916 <67279281+panda916@users.noreply.github.com>
2024-01-09 16:52:55 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -21,87 +21,105 @@ import pickle
 from matplotlib import pyplot as plt
 
 
+def creds_enter():
+    if st.session_state["user"].strip() == "admin" and st.session_state["passwd"].strip() =="admin":
+        st.session_state["authenticated"] = True
+    else:
+        st.session_state["authenticated"] = False
+        st.error("Invalid")
+
+def auth_user():
+    if "authenticated" not in st.session_state:
+        st.text_input(label = "Username: ", value = "", key = "user",on_change = creds_enter)
+        st.text_input(label = "Password: ", value = "", key = "passwd", type = "password", on_change = creds_enter)
+    else:
+        if st.session_state["authenticated"]:
+            return True
+        else:
+            st.text_input(label = "Username: ", value = "", key = "user",on_change = creds_enter)
+            st.text_input(label = "Password: ", value = "", key = "passwd", type = "password", on_change = creds_enter)
+            return False
 def main():
     
-    st.title("Upload excel file")
+    if auth_user():    
+        st.title("Upload excel file")
 
-    menu = ["Home","Dataset","DocumentFiles","About"]
-    choice = st.sidebar.selectbox("Menu",menu)
-    st.subheader("Dataset")
-    data_file = st.file_uploader("Upload excel file")
-    
-    
+        menu = ["Home","Dataset","DocumentFiles","About"]
+        choice = st.sidebar.selectbox("Menu",menu)
+        st.subheader("Dataset")
+        data_file = st.file_uploader("Upload excel file")
 
-    if st.button("Training model"):
-        if data_file is not None:
-            file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
-            st.write(file_details)
-            df = pd.read_excel(data_file)
-            start = time.time()
-            B01_01_TT_LOAD_FROM_CSV = df
-            end = time.time()
-            print(end-start)
-            B01_01_TT_LOAD_FROM_CSV.shape
-            st.dataframe(df)
-            
-            # filter out XVORL = X 
-            B01_01_TT_LOAD_FROM_CSV = B01_01_TT_LOAD_FROM_CSV[B01_01_TT_LOAD_FROM_CSV['XVORL'] != 'X'] # ok
-            # filter out ZBNKN = NULL
-            B01_01_TT_LOAD_FROM_CSV = B01_01_TT_LOAD_FROM_CSV.dropna(subset=['ZBNKN']) # ZBNKN is not null 
 
-            B01_01_TT_LOAD_FROM_CSV.shape
-            
-            ZF_COLUMNS = ['ZNME1','ZBNKN','RBETR','ZLAND','ZBNKS']
-            B01_02_TT_TRAIN_MODEL = B01_01_TT_LOAD_FROM_CSV[ZF_COLUMNS]
-            
-            B01_02_TT_TRAIN_MODEL = B01_02_TT_TRAIN_MODEL.astype(str)
-            B01_02_TT_TRAIN_MODEL['RBETR'] = pd.to_numeric(B01_02_TT_TRAIN_MODEL['RBETR'], errors='coerce')
-            
-            #Convert category to numberic value.
-            start = time.time()
-            ZF_LABEL_ENCODER = LabelEncoder()
-            for col_name in B01_02_TT_TRAIN_MODEL.columns:
-                if is_numeric_dtype(B01_02_TT_TRAIN_MODEL[col_name]) == True:
-                    pass
-                else:
-                    B01_02_TT_TRAIN_MODEL[col_name] = ZF_LABEL_ENCODER.fit_transform(B01_02_TT_TRAIN_MODEL[col_name])
-            end = time.time()
-            print(end-start)
-            
-            
-            # Note: You only run code in this cell and ignore the next cell if this is the first time you run isolation forest. If you have already trained isolation forest before and saved, run code in the next cell and ignore this cell.
 
-            # Create a Isolation Forest model that is called isf and training it with B01_02_TT_TRAIN_MODEL
-            # Note: n_estimators, max_samples, contamination, max_features should be adjusted to fit with different data set
-            start = time.time()
-            ZF_ISF_MODEL = IsolationForest(n_estimators = 500, max_samples = 500000, contamination = 0.001, max_features = 4)
-            ZF_ISF_MODEL.fit(B01_02_TT_TRAIN_MODEL)
-            end = time.time()
 
-            # Save the trained ZF_ISF_MODEL for next times. (you can edit model name)
-            ZF_MODEL_NAME_SAVE = 'modelname.sav'
-            pickle.dump(ZF_ISF_MODEL, open(ZF_MODEL_NAME_SAVE, 'wb'))
-            print(end-start)
-            
-            
-            # Load model which is trained before.
-            #ZF_MODEL_NAME_SAVE = 'modelname.sav'
-            #ZF_ISF_MODEL = pickle.load(open(ZF_MODEL_NAME_SAVE, 'rb'))
-            
-            
-            # Add result fields and Index_ field from Isolation Forest model to original data and save to SQL
-            start = time.time()
-            B01_01_TT_LOAD_FROM_CSV['Anomaly'] = pd.DataFrame(ZF_ISF_MODEL.predict(B01_02_TT_TRAIN_MODEL))
-            B01_01_TT_LOAD_FROM_CSV['Score'] = pd.DataFrame(ZF_ISF_MODEL.score_samples(B01_02_TT_TRAIN_MODEL))
-            B01_01_TT_LOAD_FROM_CSV.reset_index(inplace = True)
-            B01_01_TT_LOAD_FROM_CSV['Index_'] = B01_01_TT_LOAD_FROM_CSV['index']
-            B01_01_TT_LOAD_FROM_CSV.drop(columns = 'index', inplace = True)
-            end = time.time()
-            print(end-start)
-            st.write('Add result fields from Isolation Forest model to original data')
-            st.dataframe(B01_01_TT_LOAD_FROM_CSV)
+        if st.button("Training model"):
+            if data_file is not None:
+                file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
+                st.write(file_details)
+                df = pd.read_excel(data_file)
+                start = time.time()
+                B01_01_TT_LOAD_FROM_CSV = df
+                end = time.time()
+                print(end-start)
+                B01_01_TT_LOAD_FROM_CSV.shape
+                st.dataframe(df)
 
-            
+                # filter out XVORL = X 
+                B01_01_TT_LOAD_FROM_CSV = B01_01_TT_LOAD_FROM_CSV[B01_01_TT_LOAD_FROM_CSV['XVORL'] != 'X'] # ok
+                # filter out ZBNKN = NULL
+                B01_01_TT_LOAD_FROM_CSV = B01_01_TT_LOAD_FROM_CSV.dropna(subset=['ZBNKN']) # ZBNKN is not null 
+
+                B01_01_TT_LOAD_FROM_CSV.shape
+
+                ZF_COLUMNS = ['ZNME1','ZBNKN','RBETR','ZLAND','ZBNKS']
+                B01_02_TT_TRAIN_MODEL = B01_01_TT_LOAD_FROM_CSV[ZF_COLUMNS]
+
+                B01_02_TT_TRAIN_MODEL = B01_02_TT_TRAIN_MODEL.astype(str)
+                B01_02_TT_TRAIN_MODEL['RBETR'] = pd.to_numeric(B01_02_TT_TRAIN_MODEL['RBETR'], errors='coerce')
+
+                #Convert category to numberic value.
+                start = time.time()
+                ZF_LABEL_ENCODER = LabelEncoder()
+                for col_name in B01_02_TT_TRAIN_MODEL.columns:
+                    if is_numeric_dtype(B01_02_TT_TRAIN_MODEL[col_name]) == True:
+                        pass
+                    else:
+                        B01_02_TT_TRAIN_MODEL[col_name] = ZF_LABEL_ENCODER.fit_transform(B01_02_TT_TRAIN_MODEL[col_name])
+                end = time.time()
+                print(end-start)
+
+
+                # Note: You only run code in this cell and ignore the next cell if this is the first time you run isolation forest. If you have already trained isolation forest before and saved, run code in the next cell and ignore this cell.
+
+                # Create a Isolation Forest model that is called isf and training it with B01_02_TT_TRAIN_MODEL
+                # Note: n_estimators, max_samples, contamination, max_features should be adjusted to fit with different data set
+                start = time.time()
+                ZF_ISF_MODEL = IsolationForest(n_estimators = 500, max_samples = 500000, contamination = 0.001, max_features = 4)
+                ZF_ISF_MODEL.fit(B01_02_TT_TRAIN_MODEL)
+                end = time.time()
+
+                # Save the trained ZF_ISF_MODEL for next times. (you can edit model name)
+                ZF_MODEL_NAME_SAVE = 'modelname.sav'
+                pickle.dump(ZF_ISF_MODEL, open(ZF_MODEL_NAME_SAVE, 'wb'))
+                print(end-start)
+
+
+                # Load model which is trained before.
+                #ZF_MODEL_NAME_SAVE = 'modelname.sav'
+                #ZF_ISF_MODEL = pickle.load(open(ZF_MODEL_NAME_SAVE, 'rb'))
+
+
+                # Add result fields and Index_ field from Isolation Forest model to original data and save to SQL
+                start = time.time()
+                B01_01_TT_LOAD_FROM_CSV['Anomaly'] = pd.DataFrame(ZF_ISF_MODEL.predict(B01_02_TT_TRAIN_MODEL))
+                B01_01_TT_LOAD_FROM_CSV['Score'] = pd.DataFrame(ZF_ISF_MODEL.score_samples(B01_02_TT_TRAIN_MODEL))
+                B01_01_TT_LOAD_FROM_CSV.reset_index(inplace = True)
+                B01_01_TT_LOAD_FROM_CSV['Index_'] = B01_01_TT_LOAD_FROM_CSV['index']
+                B01_01_TT_LOAD_FROM_CSV.drop(columns = 'index', inplace = True)
+                end = time.time()
+                print(end-start)
+                st.write('Add result fields from Isolation Forest model to original data')
+                st.dataframe(B01_01_TT_LOAD_FROM_CSV)
             
 
          
